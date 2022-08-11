b4a0f052431704cd98a96455b9045dbbf00bc7a3
Denisse-Martinez <kissdmtzmtz28@gmail.com>
2022-08-10 23:42:32 -0500

    query by using SQL

--- src/main/java/com_packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com_packt/cardatabase/domain/CarRepository.java
@@ -2,15 +2,11 @@ package com_packt.cardatabase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository <Car, Long> {
-	// Fetch cars by brand
-	List<Car> findByBrand(String brand);
-
-	// Fetch cars by color
-	List<Car> findByColor(String color);
-
-	// Fetch cars by year
-	List<Car> findByYear(int year);
+	// Fetch cars by brand using SQL
+	  @Query("select c from Car c where c.brand = ?1")
+	  List<Car> findByBrand(String brand);
 }
