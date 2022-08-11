04bff97f96f4691831ebe9cb95a52e1e5bb991a1
Denisse-Martinez <kissdmtzmtz28@gmail.com>
2022-08-10 23:53:21 -0500

    extends PagingAndSortingRepository

--- src/main/java/com_packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com_packt/cardatabase/domain/CarRepository.java
@@ -1,12 +1,7 @@
 package com_packt.cardatabase.domain;
 
-import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+public interface CarRepository extends PagingAndSortingRepository <Car, Long> {
 
-public interface CarRepository extends CrudRepository <Car, Long> {
-	// Fetch cars by brand using SQL
-	  @Query("select c from Car c where c.brand = ?1")
-	  List<Car> findByBrand(String brand);
 }
