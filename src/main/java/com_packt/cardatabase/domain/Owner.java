acdab170ae31622374e49f590b3e7b88704347bd
Denisse-Martinez <kissdmtzmtz28@gmail.com>
2022-08-12 22:26:20 -0500

    Relationship in Owner with OneToMany

--- src/main/java/com_packt/cardatabase/domain/Owner.java
+++ src/main/java/com_packt/cardatabase/domain/Owner.java
@@ -1,9 +1,13 @@
 package com_packt.cardatabase.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Owner {
@@ -44,4 +48,15 @@ public class Owner {
 		this.lastname = lastname;
 	}
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="owner")
+	private List<Car> cars;
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+		
 }
